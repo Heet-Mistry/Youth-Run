f60579a40c540b5ac650f1fd81229717eb4fa344
Heet-Mistry <heetmistry@gmail.com>
2022-11-26 01:00:00 +0530

    Bug fix

--- youth.js
+++ youth.js
@@ -30,26 +30,29 @@ slidePrev.addEventListener('click', slidePrevImg);
 slideNext.addEventListener('click', slideNextImg);
 const slides = [
     {
-        image: '/youthrun/img1.jpg'
+        image: './youthrun/img1.JPG'
     },
     {
-        image: '/youthrun/img2.jpg'
+        image: './youthrun/img2.jpg'
     },
     {
-        image: '/youthrun/img3.jpg'
+        image: './youthrun/img3.jpg'
     },
     {
-        image: '/youthrun/img4.jpg'
+        image: './youthrun/img4.jpg'
     },
     {
-        image: '/youthrun/img5.jpg'
+        image: './youthrun/img5.jpg'
     }
 ];
 
 let i = 1;
 function show()
 {
-    mainPhoto.style.background = "url('" + slides[i].image + "')";
+    var path = document.location.pathname;
+var directory = path.substring(path.indexOf('/'), path.lastIndexOf('/'));
+const str = directory+slides[i].image
+    mainPhoto.style.background = `url(${str})`;
     mainPhoto.style.backgroundSize = "cover";
     mainPhoto.style.backgroundRepeat = "no-repeat";
     mainPhoto.style.borderRadius = '1rem';
